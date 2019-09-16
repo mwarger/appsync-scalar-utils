49fff87efeec66f1e99bab2f2c0d74ce3dad97de
Mat Warger <mwarger@gmail.com>
2019-09-15 19:58:21 -0500

    udpated date string

--- src/date.ts
+++ src/date.ts
@@ -1,7 +1,7 @@
 import { format, parseISO } from 'date-fns';
 
 // from docs: YYYY-MM-DDThh:mm:ss.sssZ
-export const AWSDateTimeFormatString = `yyyy-MM-dd'T'hh:mm:ss.sssX`; // date-fns format
+export const AWSDateTimeFormatString = `yyyy-MM-dd'T'hh:mm:ss.SSSX`; // date-fns format
 
 // from docs: YYYY-MM-DD
 export const AWSDateFormatString = 'yyyy-MM-dd'; // date-fns format
